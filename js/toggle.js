5e2c13aa33cbc72ed2b490a88f71d75a31f67250
ricki100 <rchitagu@gmail.com>
2019-09-15 13:16:00 +0200

    services upgraded

---
+++ js/toggle.js
@@ -0,0 +1,47 @@
+function myFunction1() {
+    var dots = document.getElementById("dot1");
+   var moreText = document.getElementById("more1");
+   var button = document.getElementById("button1");
+
+   if (dots.style.display === "none") {
+dots.style.display = "inline";
+button.innerHTML = "Read more";
+moreText.style.display = "none";
+} else {
+dots.style.display = "none";
+button.innerHTML = "Read less";
+moreText.style.display = "inline";
+}
+}
+
+function myFunction2() {
+    var dots = document.getElementById("dot2");
+   var moreText = document.getElementById("more2");
+   var button = document.getElementById("button2");
+
+   if (dots.style.display === "none") {
+dots.style.display = "inline";
+button.innerHTML = "Read more";
+moreText.style.display = "none";
+} else {
+dots.style.display = "none";
+button.innerHTML = "Read less";
+moreText.style.display = "inline";
+}
+}
+
+function myFunction3() {
+    var dots = document.getElementById("dot3");
+   var moreText = document.getElementById("more3");
+   var button = document.getElementById("button3");
+
+   if (dots.style.display === "none") {
+dots.style.display = "inline";
+button.innerHTML = "Read more";
+moreText.style.display = "none";
+} else {
+dots.style.display = "none";
+button.innerHTML = "Read less";
+moreText.style.display = "inline";
+}
+}
